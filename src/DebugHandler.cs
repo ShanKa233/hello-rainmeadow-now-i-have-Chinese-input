31dfee40e14e3b570686112354e13e063a870ef1
ShanKa <Shan_Ka@outlook.com>
2026-08-21 00:32:08 +0200

    取消debuglog

--- src/DebugHandler.cs
+++ src/DebugHandler.cs
@@ -13,7 +13,7 @@ namespace GoodMorningRainMeadow
         /// 调试开关，设置为false可以禁用所有日志输出
         /// 开着可以在BepInEx日志里确认聊天框检测是否生效，日志只在开关聊天时各一条，不会刷屏
         /// </summary>
-        public static bool EnableLogging = true;
+        public static bool EnableLogging = false;
         
         /// <summary>
         /// 日志前缀
